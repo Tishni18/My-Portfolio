4f45baa951073d924aa2acf963c5d2f7333714fa
Tishni <tishni2000@gmail.com>
2024-05-13 20:36:30 +0530

    'deleted'

--- personalportfolio/settings.py
+++ personalportfolio/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-f9!fw7@lxk($p*m9p490_*jcq@j=y*e$cks$lsz_+9t*75yui6
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['.vercel.app']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
